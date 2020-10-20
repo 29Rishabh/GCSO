2084a736475f35c6e99a2fe33b5ce43cc7f61e8a
29Rishabh <72454435+29Rishabh@users.noreply.github.com>
2020-10-20 18:30:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# GCSO
-Tells us about the strength of car
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,47 @@
+var wall,car;
+var speed,weight;
+var deformation;
+
+function setup() {
+  createCanvas(1600,400);
+  
+  
+
+  speed=random(55,90);
+
+  weight=random(400,1500);
+
+  wall=createSprite(1500, 200, 60,height/2);
+  wall.shapeColor=rgb(80,80,80);
+
+  car=createSprite(50,200,50,50);
+
+  car.velocityX=speed;
+
+  deformation=((0.5*weight*speed*speed)/22500);
+  console.log(deformation)
+}
+
+function draw() {
+  background(0);  
+  
+  if(wall.x-car.x<wall.width/2 + car.width/2){
+
+    car.collide(wall);
+
+    car.velocityX=0;
+
+    if(deformation<=100){
+      car.shapeColor=rgb(0,255,0);
+    }
+
+    if(deformation>100 && deformation<=180){
+      car.shapeColor=rgb(230,230,0);
+    }
+
+    if(deformation>180){
+      car.shapeColor=rgb(255,0,0);
+    }
+  }
+  drawSprites();
+}
